4a3910f71b11746ae634ad394fdc6f7b4f1542ba
oronibrian <brianoroni6@gmail.com>
2019-03-02 12:24:10 +0300

    App ui updates

--- app/src/main/java/com/example/oronz/mobiclientapp/MainActivity.java
+++ app/src/main/java/com/example/oronz/mobiclientapp/MainActivity.java
@@ -107,11 +107,11 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
-        else if (id == R.id.menu_logout) {
+         if (id == R.id.menu_logout) {
 
             startActivity(new Intent(getApplicationContext(),LoginActivity.class));
             SharedPreferences sharedpreferences = getSharedPreferences(LoginActivity.MyPREFERENCES, Context.MODE_PRIVATE);
